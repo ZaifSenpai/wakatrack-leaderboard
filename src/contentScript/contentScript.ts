fc94db93528e5966c2f3578347fea400bd451dc9
Zaif Senpai <escbutt@gmail.com>
2023-12-02 19:40:40 +0500

    v0.2.0
    When leaderboard is opened, scroll to row with current rank
    
    Updated extension description

--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -1,3 +1,12 @@
 ((context) => {
   const { document, window, chrome } = context;
+
+  const currentRank = document.querySelector(".current-user[class^='rank-']");
+
+  console.log("currentRank", currentRank);
+
+  currentRank?.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
 })(window);
